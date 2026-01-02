ac136e883ae23857caa81f1f73f65dc82435ae63
omarvilladiegocarrascal <denebleo08@gmail.com>
2026-01-01 19:50:59 -0500

    feat(config): add environment variables for TypeORM setup

---
+++ inventory-management-backend/src/helpers/development-env.ts
@@ -0,0 +1,9 @@
+import { config as dotenvConfig } from 'dotenv';
+
+dotenvConfig({ path: '.env' });
+
+export const DB_HOST = process.env.DB_HOST;
+export const DB_PORT = process.env.DB_PORT;
+export const DB_USERNAME = process.env.DB_USERNAME;
+export const DB_PASSWORD = String(process.env.DB_PASSWORD);
+export const DB_NAME = process.env.DB_NAME;
