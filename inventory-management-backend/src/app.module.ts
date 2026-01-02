3e47bd02ccb2b8a621bad43d061e1199bba9d613
omarvilladiegocarrascal <denebleo08@gmail.com>
2026-01-01 19:51:26 -0500

    feat(typeorm): set up TypeORM with PostgreSQL

--- inventory-management-backend/src/app.module.ts
+++ inventory-management-backend/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import typeorm from './config/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    load:[typeorm]
+  }),TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: typeorm,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
