fa4ce558b08fdb1f45306a78352e54149b91b634
omarvilladiegocarrascal <denebleo08@gmail.com>
2026-01-12 13:37:28 -0500

    chore(app): register all modules in AppModul

--- inventory-management-backend/src/app.module.ts
+++ inventory-management-backend/src/app.module.ts
@@ -5,16 +5,54 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import typeorm from './config/typeorm';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './modules/users/users.module';
+import { BatchesModule } from './modules/batches/batches.module';
+import { CostPricesModule } from './modules/cost_prices/cost_prices.module';
+import { ProductSuppliersModule } from './modules/product_suppliers/product_suppliers.module';
+import { ProductUnitModule } from './modules/product_unit/product_unit.module';
+import { ProductsModule } from './modules/products/products.module';
+import { RecipeModule } from './modules/recipe/recipe.module';
+import { RecipeProductsModule } from './modules/recipe_products/recipe_products.module';
+import { SalesModule } from './modules/sales/sales.module';
+import { SaleItemsModule } from './modules/sale_items/sale_items.module';
+import { SalePricesModule } from './modules/sale_prices/sale_prices.module';
+import { SellableItemsModule } from './modules/sellable_items/sellable_items.module';
+import { StockMovementsModule } from './modules/stock_movements/stock_movements.module';
+import { SupplierProductModule } from './modules/supplier_product/supplier_product.module';
+import { SuppliersModule } from './modules/suppliers/suppliers.module';
+import { UnitsModule } from './modules/units/units.module';
+import { UsersRefreshTokensModule } from './modules/users_refresh_tokens/users_refresh_tokens.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-    load:[typeorm]
-  }),TypeOrmModule.forRootAsync({
-    imports: [ConfigModule],
-    inject: [ConfigService],
-    useFactory: typeorm,
-  }), UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeorm],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: typeorm,
+    }),
+    UsersModule,
+    BatchesModule,
+    CostPricesModule,
+    ProductSuppliersModule,
+    ProductUnitModule,
+    ProductsModule,
+    RecipeModule,
+    RecipeProductsModule,
+    SalesModule,
+    SaleItemsModule,
+    SalePricesModule,
+    SellableItemsModule,
+    StockMovementsModule,
+    SupplierProductModule,
+    SuppliersModule,
+    UnitsModule,
+    UsersRefreshTokensModule,
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
