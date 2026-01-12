0078a985eac775ebf05c30ac0945cd5a0b9cf065
omarvilladiegocarrascal <denebleo08@gmail.com>
2026-01-12 13:38:03 -0500

    chore(main): add bearer auth configuration

--- inventory-management-backend/src/main.ts
+++ inventory-management-backend/src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -11,6 +10,7 @@ async function bootstrap() {
     .setDescription('The Inventory Management API description')
     .setVersion('1.0')
     .addTag('inventory')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
