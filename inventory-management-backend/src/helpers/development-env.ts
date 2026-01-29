2d02c63d1a8d818726e67b32887139de2e511995
omarvilladiegocarrascal <denebleo08@gmail.com>
2026-01-29 14:35:40 -0500

    chore(configuration): add DB_URL to development-envs

--- inventory-management-backend/src/helpers/development-env.ts
+++ inventory-management-backend/src/helpers/development-env.ts
@@ -7,6 +7,7 @@ export const DB_PORT = process.env.DB_PORT;
 export const DB_USERNAME = process.env.DB_USERNAME;
 export const DB_PASSWORD = String(process.env.DB_PASSWORD);
 export const DB_NAME = process.env.DB_NAME;
+export const DB_URL = process.env.DB_URL;
 export const JWT_SECRET = process.env.JWT_SECRET || 'secretKey';
 export const JWT_REFRESH_SECRET =
   process.env.JWT_REFRESH_SECRET || 'refreshSecretKey';
